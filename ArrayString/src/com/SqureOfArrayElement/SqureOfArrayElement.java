44b2ebe96778fcdcc5fc783c631b2483c835dbc2
saahilch <147494979+saahilch@users.noreply.github.com>
2024-06-30 02:13:16 +0530

    commited

--- ArrayString/src/com/SqureOfArrayElement/SqureOfArrayElement.java
+++ ArrayString/src/com/SqureOfArrayElement/SqureOfArrayElement.java
@@ -7,26 +7,24 @@ public class SqureOfArrayElement {
 
 	public static void main(String[] args) {
 
-		int arr[] = { -1, -8, -6, 2, 3, 10, 11 };
+		int arr[] = { -2, -5, -8, -9, 3, 4, 10, 23 };
 		int n = arr.length;
-		System.out.println("Befor Sort");
+		System.out.println("Before Sort");
+
 		for (int i = 0; i < n; i++)
-			System.out.print(arr[i] + " ");
-		
+			System.out.print(arr[i]+" ");
 		System.out.println("");
 		System.out.println("After Sort");
-		SortedSqure(arr);
-		
+		SortedArraySqure(arr);
 		for (int i = 0; i < n; i++)
-
-			System.out.print(arr[i] + " ");
+			System.out.print(arr[i]+" ");
 	}
 
-	public static void SortedSqure(int arr[]) {
+	public static void SortedArraySqure(int arr[]) {
 		int n = arr.length;
 		for (int i = 0; i < n; i++)
 			arr[i] = arr[i] * arr[i];
-		Arrays.parallelSort(arr);
-
+		Arrays.sort(arr);
 	}
+
 }

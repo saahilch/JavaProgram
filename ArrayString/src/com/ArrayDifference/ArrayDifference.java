5e5f27d54b2af0b92a55efd50c985f0a951af22f
saahilch <147494979+saahilch@users.noreply.github.com>
2024-07-06 02:58:13 +0530

    commited

--- ArrayString/src/com/ArrayDifference/ArrayDifference.java
+++ ArrayString/src/com/ArrayDifference/ArrayDifference.java
@@ -2,11 +2,18 @@ package com.ArrayDifference;
 
 public class ArrayDifference {
 	public static void main(String[] args) {
-		int array[] = { 2, 3,4 };
+		// declare one variable & assing array to it.
+		int array[] = { 1,2,3,4,5,6,9};
 		System.out.println("The Differenec Between Array Element IS = " + differenceOfAraayElement(array));
 
 	}
-
+	/* Create one Method & declare two variable 1-sum initially =0 to store array of length 
+	 * one for loop to traverse the array element 
+	 * again declare two var min & max & assigin both an array elements
+	 * again declare for loop to get every elemtnt of array 
+	 * then store every element into min & max
+	 * ten calculate difference between each element
+	 * */ 
 	static int differenceOfAraayElement(int array[]) {
 		int sum = 0, n = array.length;
 		for (int i = 0; i < n; i++) {
